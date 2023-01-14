43ec9ff63c634dad94e27dc33f8b7e1caaa770de
hannazo <106128732+hannazo@users.noreply.github.com>
2023-01-13 22:01:32 -0500

    added starter code

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// When mouse is over start button
+   // Button changes color
+
+// When click on start button
+   // Timer starts
+   // First question is presented
+
+// When mouse is over answer options
+   // Options change color
+
+// When click on answer
+   // Next question is presented
+   // Correct or wrong answer is displayed below answer options
+
+// If anser incorrectly
+   // Time subtracts from the clock
+
+// When aswer five questions OR timer reaches zero
+   // Page with final score is displayed
+   // Final score equals time remaining
+   // Page with initials input and submit button displayed
+   // Date is saved
+
+// When click submit button
+   // High scores page with name and score is displayed
+   // Go back button is displayed
+   // Clear high score button is displayed
+
+// When click on go back button
+   // Go to start quiz page
+
+// When click on clear high score button
+   // High score info is erased
+
+// When click on view high scores link in nav menu
+   // High scores page with name and score is displayed
